9e80ebeb25649863cd1f8fb94920f9b206d951a2
zthh626 <zthh626@gmail.com>
2020-10-17 22:54:14 -0700

    cors

--- src/flask/main.py
+++ src/flask/main.py
@@ -1,7 +1,10 @@
 """ Server ran on Pi for communication between Mobile and Pi """
 
 from flask import Flask
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
